c0dae348ab0a755c9c323c0d7f188ea069f7ad67
Jens Andersen <jensandersen@hey.com>
2022-09-08 09:20:01 +0200

    Validation for expiration month and cvc

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ paymentForm.addEventListener('submit', function (e) {
 	console.log('submitted the form');
 	console.log(e);
 	e.preventDefault();
-    // validation steps
-    const name = e.currentTarget.name.value;
+	// validation steps
+	const name = e.currentTarget.name.value;
 	const cardNumber = e.currentTarget.cardNumber.value;
 	const expirationMonth = e.currentTarget.expirationMonth.value;
 	const expirationYear = e.currentTarget.expirationYear.value;
@@ -25,18 +25,48 @@ paymentForm.addEventListener('submit', function (e) {
 	console.log('expiration year', expirationYear);
 	console.log('expiration cvc', cvc);
 
+	validateExpirationMonth(expirationMonth);
+	validateCVC(cvc);
 	showContent();
 });
 
-function validateName (name) {
-    if (!name) {
-        console.log('Sorry name cannot be empty');
-    } else if (!name){
+function validateExpirationMonth(expirationMonth) {
+	console.log('expiration month: ', expirationMonth);
+	console.log('expiration month: ', expirationMonth.length);
 
-    }
+	if (expirationMonth.length < 2) {
+		console.log('expirationMonth error: ', expirationMonth);
+		errMessage = 'Need at least 2 characters';
+		// TODO: Set error text on paragrap with error message
+		return;
+	} else if (expirationMonth < 1 || expirationMonth > 12) {
+		console.log('expirationMonth error 2: ', expirationMonth);
+		errMessage = 'Value has to be between 1 and 12';
+		// TODO: Set error text on paragrap with error message
+		return;
+	}
+
+	console.log('wuuuhu expiration month passed');
 }
 
+function validateCVC(cvc) {
+	console.log('cvc: ', cvc.length);
+	let errMessage = 'No Error';
+	if (cvc.length < 3) {
+		console.log('cvc error: ', cvc);
+		errMessage = 'Need at least 3 characters';
+		// TODO: Set error text on paragrap with error message
+		return;
+	}
+	console.log('wuuuhu cvc passed');
+}
 
+function validateName(name) {
+	if (!name) {
+		console.log('Sorry name cannot be empty');
+	} else if (!name) {
+	}
+}
 
 var wrapper = document.getElementById('wrapper');
 
